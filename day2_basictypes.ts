6354b382b9d131cc3d2194115c9e06c40042e7a6
tharun-rajeswaran <tharun.rajeswaran@chainsys.com>
2024-10-20 19:25:01 -0400

    Day 2 Commit

---
+++ day2_basictypes.ts
@@ -0,0 +1,17 @@
+//Example of String
+
+let firstName: string = 'Tharun';
+let title: string = 'Data Engineer';
+let profile: string = `I am ${firstName}. I am a ${title}.`; 
+
+console.log(profile);
+
+//Example of Object
+
+let person: object = {
+    name: "Tharun",
+    age: 24,
+    isEmployee: true
+};
+
+console.log("Person Object:", person);
